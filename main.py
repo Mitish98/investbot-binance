7e293a57968dd056f5b45e0761b92a527358d965
Matheus Iotti Crepaldi <matheus.iotti98@gmail.com>
2024-11-18 20:05:34 -0300

    add timeframe options

--- main.py
+++ main.py
@@ -71,58 +71,61 @@ async def fetch_ticker_and_candles(symbol, timeframe):
         st.error(f"Erro ao buscar dados para {symbol}: {e}")
         return None, None
 
-async def notify_conditions(symbol, timeframe):
-    """Envia notificações continuamente com base nas condições técnicas."""
+async def notify_conditions(symbol, timeframes):
+    """Envia notificações continuamente com base nas condições técnicas para múltiplos timeframes."""
     while True:  # Loop infinito
-        current_price, df = await fetch_ticker_and_candles(symbol, timeframe)
-        if df is None:
-            await asyncio.sleep(5)  # Espera 5 segundos antes de tentar novamente
-            continue
+        for timeframe in timeframes:
+            current_price, df = await fetch_ticker_and_candles(symbol, timeframe)
+            if df is None:
+                await asyncio.sleep(5)  # Espera 5 segundos antes de tentar novamente
+                continue
 
-        # Indicadores
-        df = calculate_bollinger_bands(df)
-        df = calculate_stochastic_oscillator(df)
-        rsi_indicator = RSIIndicator(df['close'], window=14)
-        df['rsi'] = rsi_indicator.rsi()
+            # Indicadores
+            df = calculate_bollinger_bands(df)
+            df = calculate_stochastic_oscillator(df)
+            rsi_indicator = RSIIndicator(df['close'], window=14)
+            df['rsi'] = rsi_indicator.rsi()
 
-        upper_band = df['upper_band'].iloc[-1]
-        lower_band = df['lower_band'].iloc[-1]
-        stochastic_k = df['%K'].iloc[-1]
-        stochastic_d = df['%D'].iloc[-1]
-        rsi = df['rsi'].iloc[-1]
-        volume_ma = df['volume'].rolling(window=21).mean().iloc[-1]
+            upper_band = df['upper_band'].iloc[-1]
+            lower_band = df['lower_band'].iloc[-1]
+            stochastic_k = df['%K'].iloc[-1]
+            stochastic_d = df['%D'].iloc[-1]
+            rsi = df['rsi'].iloc[-1]
+            volume_ma = df['volume'].rolling(window=21).mean().iloc[-1]
 
-        # Condições de compra
-        if current_price < lower_band and stochastic_k < 20 and stochastic_d < 20 and df['volume'].iloc[-1] > volume_ma and rsi < 30:
-            st.info(f"[{datetime.datetime.now()}] Sinal de COMPRA para {symbol}: Preço atual: {current_price}, RSI: {rsi:.2f}")
+            # Condições de compra
+            if current_price < lower_band and stochastic_k < 20 and stochastic_d < 20 and df['volume'].iloc[-1] > volume_ma and rsi < 30:
+                st.info(f"[{datetime.datetime.now()}] Sinal de COMPRA para {symbol} no timeframe {timeframe}: Preço atual: {current_price}")
 
-        # Condições de venda
-        if current_price > upper_band and stochastic_k > 80 and stochastic_d > 80 and df['volume'].iloc[-1] > volume_ma and rsi > 70:
-            st.info(f"[{datetime.datetime.now()}] Sinal de VENDA para {symbol}: Preço atual: {current_price}, RSI: {rsi:.2f}")
+            # Condições de venda
+            if current_price > upper_band and stochastic_k > 80 and stochastic_d > 80 and df['volume'].iloc[-1] > volume_ma and rsi > 70:
+                st.info(f"[{datetime.datetime.now()}] Sinal de VENDA para {symbol} no timeframe {timeframe}: Preço atual: {current_price}")
 
-        await asyncio.sleep(60)  # Aguarda 60 segundos antes de verificar novamente
+            await asyncio.sleep(60)  # Aguarda 60 segundos antes de verificar novamente
 
 # Configuração do Streamlit
 st.title("Robô de Notificação para Criptomoedas")
 st.write("O sistema utiliza uma combinação de indicadores técnicos para gerar sinais de compra e venda para os pares de moedas selecionados ao identificar oportunidades de mercado baseadas em condições extremas de preço e volume.")
-st.write("As condições de compra são acionadas quando o preço atual está abaixo da Banda Inferior de Bollinger, o Estocástico está abaixo de 20, o volume de negociações está acima da média móvel e o RSI (Índice de Força Relativa) está abaixo de 27, indicando uma possível sobrevenda. Por outro lado, as condições de venda são ativadas quando o preço supera a Banda Superior de Bollinger, o Estocástico está acima de 80, o volume é maior que a média e o RSI ultrapassa 73, sugerindo uma possível sobrecompra.")
-st.write("Escolha as criptomoedas que deseja receber notificação, selecione o seu timeframe de análise e clique em 'Iniciar Monitoramento'")
+st.write("As condições de compra são acionadas quando o preço atual está abaixo da Banda Inferior de Bollinger, o Estocástico está abaixo de 20, o volume de negociações está acima da média móvel e o RSI (Índice de Força Relativa) está abaixo de 30, indicando uma possível sobrevenda. Por outro lado, as condições de venda são ativadas quando o preço supera a Banda Superior de Bollinger, o Estocástico está acima de 80, o volume é maior que a média e o RSI ultrapassa 70, sugerindo uma possível sobrecompra.")
+st.write("Escolha as criptomoedas que deseja receber notificação, selecione o(s) seu(s) timeframe(s) de análise e clique em 'Iniciar Monitoramento'")
 st.sidebar.header("Configurações")
 
 # Entrada do usuário
-symbols = st.sidebar.multiselect("Selecione os pares de moedas", ["BTCUSDT", "ETHUSDT", "BNBUSDT", "DOTUSDT", "DOGEUSDT", "FTMUSDT", "ASTRUSDT", "XRPUSDT", "SOLUSDT", "LTCUSDT","PENDLEUSDT", "1000PEPEUSDT", "1000SHIBUSDT","AAVEUSDT","ORDIUSDT","UNIUSDT","LINKUSDT","ENSUSDT", "MOVRUSDT","ARBUSDT","TRBUSDT","MANTAUSDT","AVAXUSDT"])
-timeframe = st.sidebar.selectbox("Selecione o timeframe", ["1m", "5m", "15m", "1h", "4h", "1d"])
+symbols = st.sidebar.multiselect("Selecione os pares de moedas", ["BTCUSDT", "ETHUSDT", "BNBUSDT", "DOTUSDT", "DOGEUSDT", "FTMUSDT", "ASTRUSDT", "XRPUSDT", "SOLUSDT", "LTCUSDT","PENDLEUSDT", "1000PEPEUSDT", "1000SHIBUSDT","AAVEUSDT","ORDIUSDT","UNIUSDT","LINKUSDT","ENSUSDT", "MOVRUSDT","ARBUSDT","TRBUSDT","MANTAUSDT","AVAXUSDT","NEIROUSDT", "1000BONKUSDT", "1000FLOKIUSDT"])
+timeframes = st.sidebar.multiselect("Selecione o(s) timeframe(s)", ["1m", "5m", "15m", "1h", "4h", "1d"])
 
 if st.sidebar.button("Iniciar Monitoramento"):
     try:
         if not symbols:
             st.error("Por favor, selecione pelo menos um par de moedas.")
+        if not timeframes:
+            st.error("Por favor, selecione pelo menos um timeframe.")
         else:
             st.success("Monitoramento iniciado! Acompanhe os alertas abaixo.")
             loop = asyncio.new_event_loop()
             asyncio.set_event_loop(loop)
 
-            tasks = [notify_conditions(symbol, timeframe) for symbol in symbols]
+            tasks = [notify_conditions(symbol, timeframes) for symbol in symbols]
             loop.run_until_complete(asyncio.gather(*tasks))
     except Exception as e:
         st.error(f"Erro ao iniciar o monitoramento: {e}")
